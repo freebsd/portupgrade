f1a1e4365f3a68500f358b05ae1a9d9a1570504d
Sergey Matveychuk <sem@FreeBSD.org>
2006-06-30 19:47:14 +0000

    Fix portupgrade work when stdin closed.
    Requested by:	Hannes Hauswedell <hannes.hauswedell_at_@gmail.com>
    		ports/99179

--- lib/pkgtools.rb
+++ lib/pkgtools.rb
@@ -1,4 +1,4 @@
-# $Id: pkgtools.rb,v 1.3 2006/06/18 16:10:33 sem Exp $
+# $Id: pkgtools.rb,v 1.4 2006/06/30 19:16:38 sem Exp $
 
 PREFIX = "/usr/local"
 Version = "2.0.1"
@@ -486,10 +486,8 @@ alias xsystem! xsudo
 def logged_command(file, args)
   if !file  
     args
-  elsif STDIN.tty?
-    ['/usr/bin/script', '-qa', file, *args]
   else
-    ['/bin/sh', '-c', sprintf('%s >>%s 2>&1', shelljoin(*args), shelljoin(file))]
+    ['/usr/bin/script', '-qa', file, *args]
   end
 end
 
