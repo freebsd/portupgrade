90ef23a8cdf91821f1a6a8316bf223b8bc8c5adc
Stanislav Sedov <stas@FreeBSD.org>
2011-08-23 00:36:05 -0700

    - Fix several instances of :map being used on strings.

--- lib/portsdb.rb
+++ lib/portsdb.rb
@@ -185,7 +185,7 @@ class PortsDB
   def make_var(var, dir = ports_dir())
     if var.is_a?(Array)
       vars = var.join(' -V ')
-      `cd #{dir} && make -V #{vars} 2>/dev/null`.map { |val|
+      `cd #{dir} && make -V #{vars} 2>/dev/null`.lines.map { |val|
 	val.strip!
 	if val.empty?
 	  nil
