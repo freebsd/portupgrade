b0d849a3527e4f0b8f87cfe54285139c19755d46
Stanislav Sedov <stas@FreeBSD.org>
2011-08-22 23:38:34 -0700

    - Do not monkey-patch File class with realpath method, use File.expand_path   directly.

--- lib/pkgmisc.rb
+++ lib/pkgmisc.rb
@@ -94,14 +94,6 @@ def shelljoin(*args)
   }.join(' ')
 end
 
-class File
-  if not File.respond_to?('realpath') then
-    def File.realpath(path)
-      return File.expand_path(path)
-    end
-  end
-end
-
 def init_tmpdir
   if ! $tmpdir.nil? && $tmpdir != "" then
     return
