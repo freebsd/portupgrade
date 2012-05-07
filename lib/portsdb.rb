freebsd/portupgrade

commit 0978492dd93c9974239d5703d79b43ff06ab5fb9

Slightly optimize looking up INDEX/INDEXFILE when fetching/rebuilding portsdb

lib/portsdb.rb
@@ -403,7 +403,8 @@ class PortsDB
     end
 
     if fetch
-      system "cd #{abs_ports_dir} && make fetchindex && cp $(make -V INDEXDIR)/$(make -V INDEXFILE) #{tmp}"
+      index_dir, index_file = make_var(['INDEXDIR', 'INDEXFILE'])
+      system "cd #{abs_ports_dir} && make fetchindex && cp #{index_dir}/#{index_file} #{tmp}"
     else
       system "cd #{abs_ports_dir} && make INDEXFILE=INDEX.tmp index && mv INDEX.tmp #{tmp}"
     end

commit 6c2d240e917cc14591086f59db9c8c39e9771397

Fix crash on portsdb -U
This was caused by 0978492dd93c9974239d5703d79b43ff06ab5fb9 overriding
the method `index_file` with a local variable in the same scope.

- Fixes #27
- Reported by: Andrea Venturoli <ml@netfence.it>

lib/portsdb.rb
@@ -404,8 +404,7 @@ class PortsDB
     end
 
     if fetch
-      index_dir, index_file = make_var(['INDEXDIR', 'INDEXFILE'])
-      system "cd #{abs_ports_dir} && make fetchindex && cp #{index_dir}/#{index_file} #{tmp}"
+      system "cd #{abs_ports_dir} && make fetchindex && cp #{index_file} #{tmp}"
     else
       system "cd #{abs_ports_dir} && make INDEXFILE=INDEX.tmp index && mv INDEX.tmp #{tmp}"
     end