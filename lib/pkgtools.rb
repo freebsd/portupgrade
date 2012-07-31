03c9a58dc279dd4720ab5bfc40d2c3abeef2c4b0
Bryan Drewery <bryan@shatow.net>
2012-07-30 22:54:49 -0500

    Bump version

--- lib/pkgtools.rb
+++ lib/pkgtools.rb
@@ -29,10 +29,10 @@
 # $FreeBSD: projects/pkgtools/lib/pkgtools.rb,v 1.42 2011-08-18 07:36:49 stas Exp $
 
 PREFIX = "/usr/local"
-Version = "2.4.9.6"
+Version = "2.4.9.7"
 
 module PkgTools
-  DATE = '2012/07/29'
+  DATE = '2012/07/30'
 end
 
 require "pkg"
